9c9b3eb6b38958f8d726782f64c09b093f8ea3e1
SaritaMT <43161374+SaritaMT@users.noreply.github.com>
2020-08-10 14:14:12 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ClientApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxRegister = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSendServer = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxDeliveryOption = new System.Windows.Forms.ComboBox();
+            this.labelDestinationClientName = new System.Windows.Forms.Label();
+            this.textBoxDestinationClientName = new System.Windows.Forms.TextBox();
+            this.labelServerData = new System.Windows.Forms.Label();
+            this.textBoxDataFromServer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(381, 66);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(138, 23);
+            this.buttonRegister.TabIndex = 0;
+            this.buttonRegister.Text = "Register to Server";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Register Name";
+            // 
+            // textBoxRegister
+            // 
+            this.textBoxRegister.Location = new System.Drawing.Point(205, 63);
+            this.textBoxRegister.Name = "textBoxRegister";
+            this.textBoxRegister.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRegister.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter the Message";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(205, 160);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(119, 20);
+            this.textBoxMessage.TabIndex = 4;
+            // 
+            // buttonSendServer
+            // 
+            this.buttonSendServer.Location = new System.Drawing.Point(381, 206);
+            this.buttonSendServer.Name = "buttonSendServer";
+            this.buttonSendServer.Size = new System.Drawing.Size(138, 23);
+            this.buttonSendServer.TabIndex = 5;
+            this.buttonSendServer.Text = "Send To Server";
+            this.buttonSendServer.UseVisualStyleBackColor = true;
+            this.buttonSendServer.Click += new System.EventHandler(this.buttonSendServer_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select Delivery Option";
+            // 
+            // comboBoxDeliveryOption
+            // 
+            this.comboBoxDeliveryOption.FormattingEnabled = true;
+            this.comboBoxDeliveryOption.Items.AddRange(new object[] {
+            "option1",
+            "option2"});
+            this.comboBoxDeliveryOption.Location = new System.Drawing.Point(205, 208);
+            this.comboBoxDeliveryOption.Name = "comboBoxDeliveryOption";
+            this.comboBoxDeliveryOption.Size = new System.Drawing.Size(119, 21);
+            this.comboBoxDeliveryOption.TabIndex = 7;
+            this.comboBoxDeliveryOption.SelectedIndexChanged += new System.EventHandler(this.comboBoxDeliveryOption_SelectedIndexChanged);
+            // 
+            // labelDestinationClientName
+            // 
+            this.labelDestinationClientName.AutoSize = true;
+            this.labelDestinationClientName.Enabled = false;
+            this.labelDestinationClientName.Location = new System.Drawing.Point(27, 269);
+            this.labelDestinationClientName.Name = "labelDestinationClientName";
+            this.labelDestinationClientName.Size = new System.Drawing.Size(109, 13);
+            this.labelDestinationClientName.TabIndex = 8;
+            this.labelDestinationClientName.Text = "Enter the client Name";
+            // 
+            // textBoxDestinationClientName
+            // 
+            this.textBoxDestinationClientName.Enabled = false;
+            this.textBoxDestinationClientName.Location = new System.Drawing.Point(205, 266);
+            this.textBoxDestinationClientName.Name = "textBoxDestinationClientName";
+            this.textBoxDestinationClientName.Size = new System.Drawing.Size(119, 20);
+            this.textBoxDestinationClientName.TabIndex = 9;
+            // 
+            // labelServerData
+            // 
+            this.labelServerData.AutoSize = true;
+            this.labelServerData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelServerData.Location = new System.Drawing.Point(202, 9);
+            this.labelServerData.Name = "labelServerData";
+            this.labelServerData.Size = new System.Drawing.Size(106, 15);
+            this.labelServerData.TabIndex = 10;
+            this.labelServerData.Text = "Connection Status";
+            // 
+            // textBoxDataFromServer
+            // 
+            this.textBoxDataFromServer.Location = new System.Drawing.Point(205, 367);
+            this.textBoxDataFromServer.Name = "textBoxDataFromServer";
+            this.textBoxDataFromServer.Size = new System.Drawing.Size(119, 20);
+            this.textBoxDataFromServer.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Enter the data to send to server";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Data received from Server";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(381, 9);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(144, 23);
+            this.buttonDisconnect.TabIndex = 17;
+            this.buttonDisconnect.Text = "Disconnect and Close";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 450);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDataFromServer);
+            this.Controls.Add(this.labelServerData);
+            this.Controls.Add(this.textBoxDestinationClientName);
+            this.Controls.Add(this.labelDestinationClientName);
+            this.Controls.Add(this.comboBoxDeliveryOption);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonSendServer);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxRegister);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRegister);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxRegister;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSendServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxDeliveryOption;
+        private System.Windows.Forms.Label labelDestinationClientName;
+        private System.Windows.Forms.TextBox textBoxDestinationClientName;
+        private System.Windows.Forms.Label labelServerData;
+        private System.Windows.Forms.TextBox textBoxDataFromServer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonDisconnect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿//SARITA TIGADI
+//1001634958
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ClientApplication
+{
+    public partial class Form1 : Form
+    {
+        public static string serverData;      
+        TcpClient socketForServer  = new TcpClient();
+        public static NetworkStream networkStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectToServer();        
+        }
+        /// <summary>
+        ///The ConnectToServer() function connects the client to the server. The connection parameters used are "localHost" and port 10 since the
+        ///server is also configured to the same.
+        /// </summary>
+        ///
+        private void ConnectToServer()
+        {
+            labelServerData.Text = "Not Connected To Server";
+            socketForServer.Connect("localHost", 10);
+            labelServerData.Text = "Connected To Server";
+        }
+
+        /// <summary>
+        ///The buttonRegister_Click() event is invoked when the Register button is clicked.
+        ///The data is written from the specific client to the server over the networkStream.
+        ///server is also configured to the same.
+        ///The Message sent is concatenated HTTP String format
+        /// </summary>
+        ///
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            /// Reference https://www.infoworld.com/article/3016922/application-development/how-to-work-with-sockets-in-c.html
+            /// Reference https://www.codeproject.com/Questions/621289/get-http-request-header-values-in-to-csharp
+
+            networkStream = socketForServer.GetStream();
+            if (socketForServer.Connected)
+            {
+                /// Reference https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+                
+                string registerClientRequest = "{'ClientName':'" + textBoxRegister.Text + "'}";
+                HttpPOSTMessage.HttpBody = registerClientRequest;
+                HttpPOSTMessage.HttpContentLength = "Content - Length:" + registerClientRequest.Length.ToString() + "\r\n";
+               
+                byte[] requestStream = System.Text.Encoding.ASCII.GetBytes(HttpPOSTMessage.HttpRequesteType  + HttpPOSTMessage.HttpHost +
+                    HttpPOSTMessage.HttpUserAgent + HttpPOSTMessage.HttpContentType + HttpPOSTMessage.HttpContentLength + HttpPOSTMessage.HttpBody);
+
+                byte[] serverMessage = new byte[1024];
+                byte[] myWriteBuffer = requestStream;
+                networkStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
+
+                // Start a thread to listen to the Server
+                Thread newThread = new Thread(new ThreadStart(listenToServer));
+                newThread.Start();
+            }
+            else
+            {
+                labelServerData.Text = "Server Not Started";
+            }
+        }
+
+        /// <summary>
+        ///The buttonSendServer_Click() event is invoked on click of the "Send To Server" button.
+        ///The following fields have to be filled - Message , Delivery option and client Name .
+        ///These values are sent as part the data to the server. The server will be parsing these parameters to make specific 
+        ///decisions
+        /// </summary>
+        private void buttonSendServer_Click(object sender, EventArgs e)
+        {
+            ///Reference https://www.infoworld.com/article/3016922/application-development/how-to-work-with-sockets-in-c.html
+            
+            TcpClient socketForServer;
+            socketForServer = new TcpClient("localHost", 10);
+            networkStream = socketForServer.GetStream();
+            if (socketForServer.Connected)
+            {
+                string textMessage = textBoxMessage.Text.ToString();
+                string option = comboBoxDeliveryOption.Text.ToString();
+                string destinationClient = " ";
+                if (option.ToString() == "option1")
+                {                  
+                    destinationClient = textBoxDestinationClientName.Text.ToString();
+                }
+                /// Reference : https://forum.uipath.com/t/how-to-create-json-body-text-in-http-request/268/11
+                /// Reference : https://weblog.west-wind.com/posts/2015/mar/31/prettifying-a-json-string-in-net
+
+                string sendRequest = "{'ClientName':'" + textBoxRegister.Text + "','textMessage':'" + textMessage.ToString() + "','option':'" + option.ToString() + "','destinationClient':'" + destinationClient.ToString() + "'}";
+                HttpPOSTMessage.HttpBody = sendRequest;              
+                HttpPOSTMessage.HttpContentLength = "Content - Length:" + sendRequest.Length.ToString() + "\r\n";
+                byte[] requestStream = System.Text.Encoding.ASCII.GetBytes(HttpPOSTMessage.HttpRequesteType + HttpPOSTMessage.HttpHost
+                    + HttpPOSTMessage.HttpUserAgent + HttpPOSTMessage.HttpContentType + HttpPOSTMessage.HttpContentLength + HttpPOSTMessage.HttpBody);
+
+                byte[] myWriteBuffer = requestStream;
+                networkStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
+            }
+        }
+
+        /// <summary>
+        ///The comboBoxDeliveryOption_SelectedIndexChanged() enables and disables the Destination Field label and Textbox
+        ///based on whether the value is "option1" or "option2".        
+        /// </summary>
+        
+        private void comboBoxDeliveryOption_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string option = comboBoxDeliveryOption.Text.ToString();
+            if (option.ToString() == "option1")
+            {
+                labelDestinationClientName.Enabled = true;
+                textBoxDestinationClientName.Enabled = true;
+            }
+            else
+            {
+                labelDestinationClientName.Enabled = false;
+                textBoxDestinationClientName.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        ///The listenToServer() function is invoked to listen to the server when the data is sent.
+        ///The data will be sent from server in HTTP concatenated format.
+        /// </summary>
+        private void listenToServer()       
+        {
+            byte[] serverMessage = new byte[1024];
+            int numberOfBytesRead = 0;
+            string serverData;
+          
+            if (socketForServer.Connected)
+            {
+                networkStream = socketForServer.GetStream();
+                numberOfBytesRead = networkStream.Read(serverMessage, 0, serverMessage.Length);
+                serverData = Encoding.ASCII.GetString(serverMessage, 0, numberOfBytesRead);
+                string[] clientDataArray = serverData.Split('\n');               
+                serverData = clientDataArray[3].ToString();
+                textBoxDataFromServer.Text = serverData.ToString();
+            }
+        }
+
+        /// <summary>
+        ///The buttonDisconnect_Click() function is used to close the client application
+        /// </summary>
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            TcpClient socketForServer;
+            socketForServer = new TcpClient("localHost", 10);
+            networkStream = socketForServer.GetStream();
+            if (socketForServer.Connected)
+            {
+                string textMessage = "Disconnect";
+                string sendRequest = "{'ClientName':'" + textBoxRegister.Text + "','textMessage':'" + textMessage.ToString() + "'}";
+                HttpPOSTMessage.HttpBody = sendRequest;
+                HttpPOSTMessage.HttpContentLength = "Content - Length:" + sendRequest.Length.ToString() + "\r\n";
+                byte[] requestStream = System.Text.Encoding.ASCII.GetBytes(HttpPOSTMessage.HttpRequesteType + HttpPOSTMessage.HttpHost
+                    + HttpPOSTMessage.HttpUserAgent + HttpPOSTMessage.HttpContentType + HttpPOSTMessage.HttpContentLength + HttpPOSTMessage.HttpBody);
+
+                byte[] myWriteBuffer = requestStream;
+                networkStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
+                labelServerData.Text = "Disconnected To Server";
+            }
+
+            foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (myProc.ProcessName == "ClientApplication")
+                {
+                    myProc.Kill();
+                }
+            }
+            socketForServer.Close();
+        }
+    }
+
+    /// <summary>
+    ///The HttpPOSTMessage class has the HTTP fields and formats defined that will be used by the client to format the data
+    ///in HTTP String concatenated form
+    /// </summary>
+    public static class HttpPOSTMessage
+    {
+        public static string HttpHost = "Host:127.0.0.1\r\n";
+        public static string HttpUserAgent = "User-Agent:ClientApplication\r\n";
+        public static string HttpContentType = "Content-Type: text/json\r\n";       
+        public static string HttpContentLength = "";
+        public static string HttpRequesteType = "POST ServerApplication HTTP/1.1\r\n";   
+        public static string HttpBody = "";
+    }
+
+    /// <summary>
+    ///The HttpGETMessage class has the HTTP fields and formats defined that will be used by the client to format the data
+    ///in HTTP String concatenated form
+    /// </summary>
+    public static class HttpGETMessage
+    {
+        public static string HttpHost = "Host:127.0.0.1\r\n";
+        public static string HttpUserAgent = "User-Agent:ClientApplication\r\n";
+        public static string HttpContentType = "Content-Type: text/json\r\n";
+        public static string HttpContentLength = "";
+        public static string HttpRequesteType = "GET ServerApplication HTTP/1.1\r\n";
+        public static string HttpBody = "";
+    }
+}
+
